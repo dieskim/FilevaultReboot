0a07c834990f423619e21691156098b18a98cdc1
Dieskim <dieskim@julula.com>
2023-06-08 14:02:58 +0700

    Initial Commit

---
+++ FilevaultReboot/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FilevaultReboot
+//
+//  Created by Dieskim on 6/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FilevaultReboot/FilevaultRebootApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilevaultRebootApp.swift
+//  FilevaultReboot
+//
+//  Created by Dieskim on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct FilevaultRebootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
